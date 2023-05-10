d112f63330e02cbfcaf19559aa6077b9d4e62854
mmaftuhashiddiqi <mmaftuh.2021@student.uny.ac.id>
2023-05-10 20:00:41 +0700

    move css and js to external

--- admin/content-end.php
+++ admin/content-end.php
@@ -8,36 +8,6 @@
 
     <!-- Custom Javascript -->
     <script src="script.js"></script>
-    
-    <!-- Custom Javascript for sidebar -->
-    <script id="rendered-js">
-        $(".sidebar-dropdown > a").click(function() {
-            $(".sidebar-submenu").slideUp(200);
-            if (
-                $(this).parent().hasClass("active")) {
-                $(".sidebar-dropdown").removeClass("active");
-                $(this).
-                parent().
-                removeClass("active");
-            } else {
-                $(".sidebar-dropdown").removeClass("active");
-                $(this).
-                next(".sidebar-submenu").
-                slideDown(200);
-                $(this).
-                parent().
-                addClass("active");
-            }
-        });
-
-        $("#close-sidebar").click(function() {
-            $(".page-wrapper").removeClass("toggled");
-        });
-        $("#show-sidebar").click(function() {
-            $(".page-wrapper").addClass("toggled");
-        });
-        //# sourceURL=pen.js
-    </script>
 </body>
 
 </html>
--- admin/script.js
+++ admin/script.js
@@ -176,3 +176,32 @@ $(document).ready(function () {
     });
 
 });
+
+
+// Custom Javascript for sidebar
+$(".sidebar-dropdown > a").click(function () {
+    $(".sidebar-submenu").slideUp(200);
+    if (
+        $(this).parent().hasClass("active")) {
+        $(".sidebar-dropdown").removeClass("active");
+        $(this).
+            parent().
+            removeClass("active");
+    } else {
+        $(".sidebar-dropdown").removeClass("active");
+        $(this).
+            next(".sidebar-submenu").
+            slideDown(200);
+        $(this).
+            parent().
+            addClass("active");
+    }
+});
+
+$("#close-sidebar").click(function () {
+    $(".page-wrapper").removeClass("toggled");
+});
+
+$("#show-sidebar").click(function () {
+    $(".page-wrapper").addClass("toggled");
+});
